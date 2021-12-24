84e716e5aac379754f9cb59bdc8bea5e9cf56d7c
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2021-12-24 10:06:04 +0530

    Update lab1.py

--- Lab-1-PacmanIsUninformed/4/lab1.py
+++ Lab-1-PacmanIsUninformed/4/lab1.py
@@ -1,1 +1,36 @@
 # This is the main code file
+
+import sys
+
+k = 0
+method = 0
+fullMatrix = []
+lnNum = 0
+colNum = 0
+class node:
+    pass
+input_grPH = sys.stdin.readlines()
+
+for line in input_grPH:  
+    lnNum+=1
+    matrixline = []
+    if k == 0 :
+        k = 1
+        method = int(line)
+        continue
+    for char in line:
+        colNum+=1
+        if char == ' ':
+            matrixline.append(0)
+        if char == '|' or char == '-' or char == '+':
+            matrixline.append(1)
+        if char == '*':
+            goalState = (lnNum, colNum)
+            matrixline.append(2)
+    fullMatrix.append(matrixline)
+    fullMatrix[0][0] = 0
+for each_line in fullMatrix:
+    print(each_line)
+    
+
+    
